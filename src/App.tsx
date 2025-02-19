efe1245470b2647d4808629e0b166d903f84fa5e
saad172 <63830943+saad172@users.noreply.github.com>
2025-02-19 01:02:42 -0600

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
 import Gallery from './components/Gallery';
 import ContactForm from './components/ContactForm';
 import VideoSection from './components/VideoSection';
+import Footer from './components/Footer';
 
 const App = () => {
 
@@ -17,6 +17,7 @@ const App = () => {
       <Features />
       <Gallery />
       <ContactForm />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-button text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+          Created with <span className="text-red-500">♥</span> by 
+          <a href="https://www.consultmsgroup.com" target="_blank" rel="noopener noreferrer" className="text-primaryHover hover:underline ml-1">
+            M&S Consulting Group
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer; 
